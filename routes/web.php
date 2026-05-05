6b77228d8749ba304b25fa3dcc8a6d3136285ea9
Birendra Gurung <birendragurung@users.noreply.github.com>
2026-05-05 18:51:25 +0545

    add hello world test

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hello', function () {
+    return ['data' => 'world'];
+});
+
 Route::get('/users', function () {
     $users = User::query()->limit(100)->get();
 
